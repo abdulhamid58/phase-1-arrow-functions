7b211dcfd7ca0b8965b7eb4d0afc10ec04a957e6
Abdulhamid <abdulhamidhersi@gmail.com>
2024-07-04 11:54:00 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,19 @@
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+const twoAdder = x => x + 2;
+
+const sum = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+};
+
+const squaredNumbers = [1, 2, 3].map(x => x ** 2);
+
+const divide = (dividend, divisor) => dividend / divisor;
+
+const square = x => x * x;
+
+
+
+
