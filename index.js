d25ae4c8d345908b068464f5bbe6be363c62c828
Abdulhamid <abdulhamidhersi@gmail.com>
2024-09-18 09:41:47 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const add = (parameter1, parameter2) => parameter1 + parameter2;
+add .appcc
 
-const twoAdder = x => x + 2;
-
-const sum = (parameter1, parameter2) => {
-    console.log(`Adding ${parameter1}`);
-    console.log(`Adding ${parameter2}`);
-    return parameter1 + parameter2;
-};
-
-const squaredNumbers = [1, 2, 3].map(x => x ** 2);
-
-const divide = (dividend, divisor) => dividend / divisor;
-
-const square = x => x * x;
+const divide = (num1, num2) =>  num1 / num2;
+const square =(a) => a * a
+const add = (a, b) => a + b;
 
 
 
